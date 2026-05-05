c6caa7a70b2162f1749af07c53ffbde9d7d3ad1e
Hari9704 <79081901+Hari9704@users.noreply.github.com>
2026-05-05 13:24:58 +0530

    Fix: remove GoogleOAuthProvider, add ToastProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ToastProvider } from './components/Toast.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'YOUR_GOOGLE_CLIENT_ID'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <ToastProvider>
-        <App />
-      </ToastProvider>
-    </GoogleOAuthProvider>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
 )
